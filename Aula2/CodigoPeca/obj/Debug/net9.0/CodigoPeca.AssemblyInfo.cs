1797805c5d2ea6f675d432d91b6602574cc05b6c
YanGabardo <yan.gabardo@gmail.com>
2026-03-24 10:23:38 -0300

    aula5

--- Aula2/CodigoPeca/obj/Debug/net9.0/CodigoPeca.AssemblyInfo.cs
+++ Aula2/CodigoPeca/obj/Debug/net9.0/CodigoPeca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodigoPeca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+699f2b499b472265dba2a1255acae15a30c88046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bea752a99c7b8631f7d2874a629c923a39cea097")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodigoPeca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodigoPeca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Aula5/media/Program.cs
@@ -0,0 +1,26 @@
+﻿int n = int.Parse(Console.ReadLine());
+int contador = 0;
+double soma = 0;
+int i = 1;
+
+while (i <= n)
+{
+    int numero = int.Parse(Console.ReadLine());
+    if (numero % 2 == 0) //verifica se o número é par
+    {
+        soma += numero;
+        contador++;
+    }
+
+    i++;
+}
+
+if (contador > 0) //verifica se houve algum número par digitado
+{
+    double media = soma / contador;
+    Console.WriteLine("Média: " + media);
+}
+else
+{
+    Console.WriteLine("Nenhum numero par foi digitado");
+}
